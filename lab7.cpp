8c30693ed2714f4158e4391361a30dba9fccb417
shwaliov13andriy <andrei.shwalev@gmail.com>
2024-12-14 19:26:30 +0200

    Update lab7.cpp

--- lab7.cpp
+++ lab7.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void removeDuplicates(int* arr, int& size) {
+    int uniqueCount = 0;
+    for (int i = 0; i < size; ++i) {
+        bool isDuplicate = false;
+        for (int j = 0; j < uniqueCount; ++j) {
+            if (arr[i] == arr[j]) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (!isDuplicate) {
+            arr[uniqueCount++] = arr[i];
+        }
+    }
+    size = uniqueCount;
+}
+int main() {
+    int size;
+    cout << "Type size: ";
+    cin >> size;
+    int* arr = new int[size];
+
+    cout << "Type elements: ";
+    for (int i = 0; i < size; ++i) {
+        cin >> arr[i];
+    }
+    removeDuplicates(arr, size);
+    cout << "Unique elements: ";
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    delete[] arr;
+
+    return 0;
+}
 
